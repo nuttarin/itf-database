a201079fcbaadba457f72e053b2ff50e71c2d457
nuttarin <73684062+nuttarin@users.noreply.github.com>
2020-11-19 05:44:12 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<head>
+<title>ITF Lab</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itflab-13.mysql.database.azure.com', 'nuttarin@itflab-13', 'Nan312615', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1" class="table table-striped table-dark">
+  <tr class="bg-info">
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['name'];?></div></td>
+    <td><?php echo $Result['comment'];?></td>
+    <td><?php echo $Result['link'];?></td>
+    <td>
+      <a href="edit.php?id=<?php echo $Result['id']?>" class="btn btn-outline-success" >EDIT</a>
+      <a href="del.php?id=<?php echo $Result['id']?>" class="btn btn-outline-danger"onclick="return confirm('Confirm data deletion?')">DELETE</a>
+    </td>
+  </tr>
+<?php
+}
+?>
+</table>
+<button type="button" class="btn btn-outline-warning" onclick ="window.location.href='form.html'">ADD</button>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
